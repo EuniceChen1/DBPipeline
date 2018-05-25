d28ad9f042329aa0fe5d66a28c2642f771f17469
Eunice Chen <eunicechen93@gmail.com>
2018-05-25 11:03:54 +0800

    Update main.py

--- main.py
+++ main.py
@@ -40,6 +40,7 @@ class Window(QtGui.QMainWindow,Ui_MainWindow):
             
         self.seqTree.expandItem(QtGui.QTreeWidgetItem(self.sqName))
         self.seqTree.addTopLevelItems(self.sqName)
+        self.seqTree.sortItems(0,Qt.SortOrder(0))
 
     def addSelBt(self):
         self.noSubItem = self.subItemBox.text()
@@ -80,12 +81,12 @@ class Window(QtGui.QMainWindow,Ui_MainWindow):
     def clearBt(self):
         selectedRemove = self.seqTree.selectedItems()
         if len(selectedRemove) > 0:
-            #self.seqTree.clearSelection()
             selectedRemove[0].takeChildren()
         else:
             assert (len(selectedRemove) > 0), "Nothing is selected!"
 
     def makeCSV(self):
+        sq=[]
         outputFileA = os.path.join("D:\\"+"temp"+".csv")
         header = ["Sequence","Scene","Shot"]
         with open(outputFileA,'wb') as g:
@@ -93,30 +94,46 @@ class Window(QtGui.QMainWindow,Ui_MainWindow):
             wr = csv.DictWriter(g,fieldnames=fieldnames)
             wr.writeheader()
 
-##            keep_running = True
-##            while(keep_running):
-            
             for j in self.sqName:
-                #print "sequence :",j.text(0)
+                sq.append(j.text(0).split("q")[-1])
                 for scItem in range(0,j.childCount()):
                     scItem = scItem + 1
                     if j.child(scItem-1).text(0) != None:
-                        #print "scene :",j.child(scItem-1).text(0)
                         scene = j.child(scItem-1)
                         
                         for shItem in range(0,scene.childCount()):
                             shItem = shItem + 1
                             if scene.child(shItem-1).text(0) != None:
-                                #print "shot :", scene.child(shItem-1).text(0)
                                 shot = scene.child(shItem-1)
 
                                 wr.writerow({
-                                            fieldnames[0]:j.text(0),
-                                            fieldnames[1]:scene.text(0),
+                                            fieldnames[0]:j.text(0).split("q")[-1],
+                                            fieldnames[1]:scene.text(0).split("c")[-1],
                                             fieldnames[2]:shot.text(0)
                                             })
-                    
+                                
+        projName = self.projNameBox.text()
+        outputFileB = os.path.join("D:\\"+projName+"_"+"tacticData"+".csv")
+        print len(sq)
+        print len(self.sqList)
+        print len(self.scnName)
+        
+        with open(outputFileA,'r') as inf:
+            with open(outputFileB,'wb') as outf:
+                reader = inf.readlines()
+                for ind, q in enumerate(reader):
+                    if ind == 0:
+                        reader[ind] = "Sq,"+q
+                    else:
+                        if ind-1 >= len(sq):
+                            reader[ind] = ","+q
+                        else:
+                            reader[ind] = self.sqList[ind-1]+","+q
 
+                    outf.write(reader[ind])
+                    
+        os.remove(outputFileA)
+                            
 
 if __name__ == "__main__":
     app = QtGui.QApplication(sys.argv)
